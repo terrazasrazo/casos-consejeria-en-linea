b65b57b75164f27129a420aa0e63d50cea4e27f6
Omar Terrazas <terrazas.omar@gmail.com>
2023-09-14 12:59:51 -0600

    Se cambia url del sitio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/casos-consejeria-en-linea/',
+  base: '/casosconsejeria/',
   plugins: [react()],
 })
